be5ff1b29c772a221aaa779328e95b36ef15bc9a
Westley Strellis <wstrellis@gmail.com>
2021-05-14 12:44:32 -0400

    updated script to deploy from github

--- deploy_to_azure.sh
+++ deploy_to_azure.sh
@@ -3,6 +3,7 @@
 APP_NAME=$1
 RESOURCE_GROUP="$APP_NAME-rg"
 APP_SERVICE_PLAN="$APP_NAME-asp"
+DEPLOYMENT_URL=$2
 
 echo "App name is: $APP_NAME"
 echo "RESOURCE_GROUP is $RESOURCE_GROUP"
@@ -14,19 +15,31 @@ az group create \
 --location eastus \
 --name $RESOURCE_GROUP
 
+# Create App Service Plan
+echo "Creating app service plan: $APP_SERVICE_PLAN"
+az appservice plan create \
+ --name $APP_SERVICE_PLAN\
+ --resource-group $RESOURCE_GROUP \
+ --is-linux \
+ --location eastus \
+ --sku P1V2 
+
+
 # Create web app
 echo "Creating web app"
-az webapp up \
+az webapp create \
+ --name $APP_NAME \
+ --resource-group $RESOURCE_GROUP \
+ --plan $APP_SERVICE_PLAN \
+ --deployment-source-branch main \
+ --deployment-source-url $DEPLOYMENT_URL \
+ --runtime "NODE|14-lts" 
+ 
+# Start web app
+echo "Starting web app"
+az webapp start \
 --name $APP_NAME \
---resource-group $RESOURCE_GROUP \
---plan $APP_SERVICE_PLAN \
---runtime "NODE|14-lts" \
---sku P1V2 \
---os-type Linux \
---location eastus
+ --resource-group $RESOURCE_GROUP \
+ --slot production 
 
-# APPNAME=$(az webapp list --query [0].name --output tsv)
-# APPRG=$(az webapp list --query [0].resourceGroup --output tsv)
-# APPPLAN=$(az appservice plan list --query [0].name --output tsv)
-# APPSKU=$(az appservice plan list --query [0].sku.name --output tsv)
-# APPLOCATION=$(az appservice plan list --query [0].location --output tsv)
+
