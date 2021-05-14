296b1cb1f95d5e883af4e2a8e1067138483ad1a9
Westley Strellis <wstrellis@gmail.com>
2021-05-14 13:11:41 -0400

    error checks in deployment script

--- deploy_to_azure.sh
+++ deploy_to_azure.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ $# -lt 2 ]; then
+    echo "Need app name and github url"
+    exit 1
+fi
+
 APP_NAME=$1
 RESOURCE_GROUP="$APP_NAME-rg"
 APP_SERVICE_PLAN="$APP_NAME-asp"
@@ -8,12 +13,16 @@ DEPLOYMENT_URL=$2
 echo "App name is: $APP_NAME"
 echo "RESOURCE_GROUP is $RESOURCE_GROUP"
 echo "APP_SERVICE_PLAN is $APP_SERVICE_PLAN"
+echo "DEPLOYMENT_URL is $DEPLOYMENT_URL"
 
 # Create Resource Group
 echo "Creating resource group: $RESOURCE_GROUP"
 az group create \
 --location eastus \
 --name $RESOURCE_GROUP
+if [ $? -gt 0 ]; then 
+     exit 1
+fi
 
 # Create App Service Plan
 echo "Creating app service plan: $APP_SERVICE_PLAN"
@@ -24,6 +33,9 @@ az appservice plan create \
  --location eastus \
  --sku P1V2 
 
+if [ $? -gt 0 ]; then 
+     exit 1
+fi
 
 # Create web app
 echo "Creating web app"
@@ -34,12 +46,16 @@ az webapp create \
  --deployment-source-branch main \
  --deployment-source-url $DEPLOYMENT_URL \
  --runtime "NODE|14-lts" 
- 
+if [ $? -gt 0 ]; then 
+    exit 1
+fi
+
 # Start web app
 echo "Starting web app"
 az webapp start \
 --name $APP_NAME \
  --resource-group $RESOURCE_GROUP \
  --slot production 
-
-
+if [ $? -gt 0 ]; then 
+    exit 1
+fi
