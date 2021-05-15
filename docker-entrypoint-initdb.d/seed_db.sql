53f8bb37819c48d8712874fcf64c3d44d9c37701
Westley Strellis <wstrellis@gmail.com>
2021-05-15 17:18:24 -0400

    uses a database

---
+++ docker-entrypoint-initdb.d/seed_db.sql
@@ -0,0 +1,10 @@
+USE node_app;
+
+CREATE TABLE IF NOT EXISTS shopping_list (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    item VARCHAR(255) NOT NULL,
+    quantity SMALLINT NOT NULL DEFAULT 1
+);
+
+INSERT INTO shopping_list (item, quantity)
+VALUES ("celery", DEFAULT),("carrots", DEFAULT),("apples",7);
--- server.js
+++ server.js
@@ -1,7 +1,46 @@
 const express = require('express')
+const mysql = require('mysql2/promise')
+require('dotenv').config()
+
 const server = express()
 
+// server.use('/api',express.json())
+
+var mysqlConfig =
+{
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
+    port: process.env.MYSQL_PORT || 3306
+};
+
+// console.log(mysqlConfig)
+
+
+
+async function useDB(cb) {
+    const mysqlConn = await mysql.createConnection(mysqlConfig)
+    const [rows] = await cb(mysqlConn)
+    await mysqlConn.end()
+    return rows
+}// end useDB
+
+function readData(db){
+    return db.query('SELECT * FROM shopping_list') 
+};
+
 let port = Number(process.env.PORT) || 3030
+
+server.get("/api/shopping", async (req,res) => {
+    try {
+       result = await useDB(readData) 
+       res.status(200).json(result)
+    } catch (error) {
+       console.error(error)
+       res.status(400).json({error:"An error occured"})  
+    }
+})
 
 server.use('/',express.static('web'))
  
