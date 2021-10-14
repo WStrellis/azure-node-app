3058ea2bb6120d11fe4d02df835cdf190eb1e571
Westley Strellis <wstrellis@gmail.com>
2021-10-14 08:23:01 -0400

    have build stage

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,25 +1,37 @@
+import {useEffect,useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ShowTime/>
       </header>
     </div>
   );
 }
 
+function useTime(delay=200){
+  const [intervalID,setIntervalID] = useState(null)
+  const [ currentTime,setCurrentTime ] = useState(new Date())
+
+  useEffect(()=>{
+   if (intervalID === null){
+    setIntervalID(setInterval(()=>{
+      setCurrentTime(new Date())
+    },delay) )
+   }
+   return () => clearInterval(intervalID)
+  }, [intervalID,delay])
+
+  return currentTime
+}
+
+function ShowTime(){
+  const current = useTime()
+  return <span>{current.toLocaleString()}</span>
+}
 export default App;
